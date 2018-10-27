f2857ae4799004d37642232a6bef0d36ec9935f5
Ricardo MG Silva <up201607780@fe.up.pt>
2018-10-27 23:19:48 +0100

    up

--- clobber/clobberDisplay.pl
+++ clobber/clobberDisplay.pl
@@ -8,8 +8,9 @@ initialBoard([
     [white,black,black,black,white]
 ]).
 
+
 symbol(empty,S) :- S ='.'.
-symbol(black,S) :- char_code(S,9863). 
+symbol(black,S) :- char_code(S,9863).  
 symbol(white,S) :- char_code(S,9865). 
 
 %char_code(Char,9863), write(Char).
@@ -20,8 +21,6 @@ printBoard(X):- nl, initialBoard(X), write('\n   | A | B | C | D | E |\n'), writ
 
 printMatrix([], 6).
 
-%letter(N, L),
-%write(L), write('\n  | A | B |C | D | E |\n'),
 printMatrix([Head|Tail], N) :-   
     write(' '),   
     N1 is N + 1,
@@ -31,11 +30,26 @@ printMatrix([Head|Tail], N) :-
     write('\n   |---|---|---|---|---|\n'),
     printMatrix(Tail, N1).
 
+%caso base
 printLine([]).
-
 printLine([Head|Tail]) :-
     symbol(Head, S),
     write(S),
     write(' | '),
     printLine(Tail).
 
+
+tabuleiroInicial(Board,getPeca(+Linha,+Coluna,+Tabuleiro,-Peca)).
+tabuleiroInicial(Board,setPeca(+Linha,+Coluna,+Tabuleiro,+Peca,-TabOut)).
+
+validMove(estadoTotal(tab, jogador),jogada,novoTab, novoJogo).
+
+%para ter a avaliacao do tabuleiro
+evalBoard(Tab,J, val).
+
+%para termos todas as jogadas validas
+%depois eh so usar a random library
+findall(jog, validMove( tab, j , jog, NT, NJ ), L).
+
+%val_jog eh o valor da jogada avaliado o tabuleiro apos a jogada
+setOf(val_jog, (validMove(tab,J,jog,NT,NJ),evalBoard(Tab,J, val) ), L ).
