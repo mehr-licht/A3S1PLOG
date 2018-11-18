2ed72877de0247798fd1a344aeb63f30530ab069
Mehr Licht! <mehrlicht@gmail.com>
2018-11-18 13:05:27 +0000

    Reintroduced documentation to botsLogic.pl

--- clobber/botsLogic.pl
+++ clobber/botsLogic.pl
@@ -1,7 +1,11 @@
-
 %###############################################################################   base do findall Eureka!
 /**
  * Devolve Index da linha onde esta a peca com a magia negra do backtracking
+* getValueFromListRenew(+Lista, +Index, -Value)
+* @brief percorre lista(linha) e devolve valor em determinado indice(celula)
+* @param +Lista linha da matriz(tabuleiro)
+* @param +Index indice na linha(celula)
+* @param -Value qual a peca que esta na celula pesquisada
 */
 getValueFromListRenew([H|T], Index, Value) :-
     nth0(Index,[H|T],Value).
@@ -9,18 +13,24 @@ getValueFromListRenew([H|T], Index, Value) :-
 * Uma matrix eh uma lista de listas -> wooahh Sherlock
 * Percorre uma linha em primeiro lugar, e depois com o getValueFrom List retorna o Valor
 * Caso base quando estamos na ultima linha
+*
+ * getValueFromMatrixRenew(+Lista, +Row, +Column, -Value)
+* @brief percorre matriz(tabuleiro) e devolve valor em determinada celula, chamando getValueFromListV2 depois de ter chegado a coluna em causa
+* @param +Lista matriz(tabuleiro)
+* @param +Row indice da linha
+* @param +Column indice da coluna
+* @param -Value qual a peca que esta na celula pesquisada
 */
 getValueFromMatrixRenew([H|_T], 0, Column, Value) :-
     getValueFromListRenew(H, Column, Value).
-
-/**Devolve o numero de linha no backtracking de volta para juntar ao par de coordenadas
- * Admiravel mundo novo
-*/
-getValueFromMatrixRenew([_H|T], Row1, Column, Value) :-
-    getValueFromMatrixRenew(T, Row, Column, Value),
     Row1 is Row +1.
 
 /**
+ * selecionarPecaForBot(+TabuleiroInicial, -LinhaEColuna, +ColorPlayer)
+ * @ brief Verifica se a peca escolhida eh da cor passada
+ * @param +TabuleiroInicial: tabuleiro actual
+ * @param +LinhaEColuna: celula como par
+ * @param +ColorPlayer: cor a verificar
  * Verifica se a peca escolhida eh preta
  * selecionarPecaForBot(TabuleiroInicial, [LineIndex-ColumnIndex], ColorPlayer)
 */
@@ -31,13 +41,24 @@ selecionarPecaForBot(TabuleiroInicial, [LineIndex-ColumnIndex], ColorPlayer):-
 %#########################################################################################
 
 /** 
- * jogadasPossiveis(+TabuleiroInicial,+Color,-ListaDePares)
+ * posicoesPecasNoTabuleiro(+TabuleiroInicial,+Color,-ListaDePares)
+ * @brief Devolve todas as posicoes das pecas de Color actualmente existentes no tabuleiro
+ * @param TabuleiroInicial: tabuleiro actual
+ * @param Color: cor da peca
+ * @param ListaDePares: posicoes de todas as pecas de Color actualmente no tabuleiro
  * Devolve todas as posicoes das pecas no tabuleiro
 */
 posicoesPecasNoTabuleiro(TabuleiroInicial,Color,ListaDePares):-
     findall([LineIndex-ColumnIndex], selecionarPecaForBot(TabuleiroInicial,[LineIndex-ColumnIndex], Color), ListaDePares).
 
-/**
+/** 
+ * jogadasNaPosicaoPossiveis(+Tabuleiro,+LineIndex,+ColumnIndex,+Color,-NewList)
+ * @brief Devolve lista com todas as jogadas possiveis validas para as pecas de color actualmente existentes no tabuleiro
+ * @param +Tabuleiro: tabuleiro actual
+ * @param +LineIndex: indice da linha
+ * @param +ColumnIndex: indice da coluna
+ * @param +Color: cor da peca
+ * @param -NewList:  lista com todas as jogadas possiveis validas para as pecas de color actualmente existentes no tabuleiro
  * Devolve as jogadas na PosicaoPossiveis
  * jogadasNaPosicaoPossiveis(+TabuleiroInicial, +LineIndex, +ColumnIndex, +ColorPlayer, -ListasJogadas ) 
 */
@@ -63,7 +84,12 @@ cleanLista([[Line-Column]-SizeLista|ListaFinal], NovaLista):-
 
 
 /**
- * direccaoDaJogada(+Tabuleiro,+[Line-Column],FuturaJogada, -ListaJogadasVizinhas) 
+ * direccaoDaJogada(+Tabuleiro,+LinhaEColuna,-Color, -ListaJogadasVizinhas)
+ * @brief escolha a jogada seguinte do bot
+ * @param +Tabuleiro: tabuleiro actual
+ * @param +LinhaEColuna: celula vizinha
+ * @param -Color: cor da peca na celula vizinha
+ * @param -ListaJogadasVizinhas: jogadas possiveis na celula vizinha
 * Devolve todas as solucoes possiveis para aqueler valor
 */
 direccaoDaJogada(Tabuleiro,[Line-Column],Color, ListaJogadasVizinhas):-
@@ -73,16 +99,19 @@ posicaoPecasPretas([[1-1],[1-3],[2-2],[2-4],[3-3],[5-1]]).
 posicaoPecasBrancas([[1-2],[2-1],[3-0],[3-1],[4-1],[4-3]]).
 
 /**
+ * jogarLeBot(+TabuleiroInicial, -TabuleiroFinal)
  * @brief Generates a random play for the bot without being clever - a black piece eats a white one
+ * @param +TabuleiroInicial: tabuleiro actual
+ * @param -TabuleiroFinal: tabuleiro futuro
 */
 jogarLeBot(Tabuleiro, TabuleiroFinal):-
     posicoesPecasNoTabuleiro(Tabuleiro,black,ListaDePares),
     escolha(Tabuleiro, ListaDePares,ListaParaLimpar),
-    cleanLista(ListaParaLimpar, NovaLista), %Novalista formato [[1-1]-2,[1-3]-1,[2-2]-2,[3-3]-1,[5-1]-1]
+    cleanLista(ListaParaLimpar, _NovaLista), %Novalista formato [[1-1]-2,[1-3]-1,[2-2]-2,[3-3]-1,[5-1]-1]
    % <-- vouaqui->
-    direccaoDaJogada(Tabuleiro,[Line-Column],white, ListaJogadasVizinhas),
+    direccaoDaJogada(Tabuleiro,[Line-Column],white, _ListaJogadasVizinhas),
 %    escolha(ListaJogadasVizinhas,NewLineIndex,NewColumnIndex),
-    replaceInMatrix(TabuleiroInicial, NewLineIndex, NewColumnIndex, black, TabuleiroNovo),
+    replaceInMatrix(_TabuleiroInicial, NewLineIndex, NewColumnIndex, black, TabuleiroNovo),
     replaceInMatrix(TabuleiroNovo, Line, Column, empty, TabuleiroFinal),
     write('Jogou de '),
     write(Line), 
@@ -108,8 +137,15 @@ jogarLeBot(TabuleiroInicial, TabuleiroFinal):-
 
 
 /**
-* Selecciona o Sul, Norte, Oeste, Este,  sequencialmente consoante as jogadas falharem
-* seleccionarBotJogada(+TabuleiroInicial, +LineIndex, +ColumnIndex, -NewLineIndex, -NewColumnIndex, ColorPlayer )
+* 
+* seleccionarBotJogada(+TabuleiroInicial, +LineIndex, +ColumnIndex, -NewLineIndex, -NewColumnIndex, +ColorPlayer )
+* @ Selecciona o Sul, Norte, Oeste, Este,  sequencialmente consoante as jogadas falharem
+* @param +TabuleiroInicial: tabuleiro actual
+* @param +LineIndex: indice da linha actual da celula
+* @param +ColumnIndex: indice da coluna actual da celula
+* @param -NewLineIndex: indice da nova linha da celula
+* @param -NewColumnIndex: indice da nova coluna da celula
+* @param +ColorPlayer: para verificar que a celula escolhido tem adversario
 */
 %sul
 seleccionarBotJogada(TabuleiroInicial, LineIndex, ColumnIndex, NewLineIndex, NewColumnIndex, ColorPlayer ):-
